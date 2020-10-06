beca37ce746a56ede53664e558ec99c4d7d444a5
Timur <spilex.dev@gmail.com>
2020-10-07 01:05:13 +0300

    Lab4_15_2

---
+++ Lab 4/Lab4_15_2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int kv, vsepadiki = 4, vseetazhy = 9, kvnaetazhe = 4;
+	cin >> kv;
+	int padik = (kv - 1) / (vsepadiki * vseetazhy) + 1;
+	int etazh = ((kv - 1) % (vsepadiki * vseetazhy)) / kvnaetazhe + 1;
+	cout << "padik = " << padik << endl;
+	cout << "etazh = " << etazh << endl;
+}
