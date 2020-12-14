1f63fe42aaa5222280080fdf4108e2dca1c1519a
Timur <spilex.dev@gmail.com>
2020-12-15 01:04:02 +0300

    Lab11_11_1

---
+++ Lab 11/Lab11_11_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+using namespace std;
+int main()
+{
+	int A; 
+	char tmp[33];
+	cout << "Vvedite chislo: ";
+	cin >> A;
+	_itoa_s(A, tmp, 2);
+	cout << "A v dvoichnom vide = " << tmp << endl;
+	if ((A & 15) == 0)
+		cout << "Chislo kratno 16" << endl;
+	else
+		cout << "Chislo ne kratno 16" << endl;
+}
