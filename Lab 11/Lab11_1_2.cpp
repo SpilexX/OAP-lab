e3632db20d0b6e88dff5af050885275bf9820929
Timur <spilex.dev@gmail.com>
2020-12-15 06:29:28 +0300

    Lab11_1_2: Cleanup

--- Lab 11/Lab11_1_2.cpp
+++ Lab 11/Lab11_1_2.cpp
@@ -1,25 +1,26 @@
 ﻿#include <iostream>
 using namespace std;
 
+/*
+*	Инвертировать в числе А n битов вправо от позиции p, 
+*	заменить ими m битов числа В, начиная с позиции q.
+*/
+
 int main()
 {
 	setlocale(LC_CTYPE, "Russian");
-	int A = 178, B = 128, p = 5, n = 4, q = 6, m = 5;
+	int A, B, p, n, q, m;
 	char tmp[33];
-	/*
 	cout << "введите A "; cin >> A;
 	cout << "введите B "; cin >> B;
-	*/
 	cout << "A = ";	_itoa_s(A, tmp, 2);
 	cout << tmp << endl;
 	cout << "B = ";	_itoa_s(B, tmp, 2);
 	cout << tmp << endl;
-	/*
 	cout << "введите нач. позицию A "; cin >> p;
 	cout << "введите кол-во битов A "; cin >> n;
 	cout << "введите нач. позицию B "; cin >> q;
 	cout << "введите кол-во битов B "; cin >> m;
-	*/
 
 	int Amask = pow(2, n) - 1;
 	Amask <<= p - n;
