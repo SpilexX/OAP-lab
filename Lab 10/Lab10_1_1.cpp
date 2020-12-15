9be456867041e9addfde55f415c14d1ec6f85a15
Timur <spilex.dev@gmail.com>
2020-12-15 06:21:16 +0300

    Lab10_1_1: minor changes

--- Lab 10/Lab10_1_1.cpp
+++ Lab 10/Lab10_1_1.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
-int main() 
-{   using namespace std;
+﻿#include<iostream>
+int main()
+{
+    using namespace std;
     setlocale(LC_CTYPE, "Russian");
-    int size, Del_Num; 
+    int size, Del_Num;
     int Col = 0;
-    int Even_Num = 0; 
+    int Even_Num = 0;
     cout << "Введите размерность массива: ";
     cin >> size;
-    int* mass = new int[size]; 
-    
+    int* mass = new int[size];
+
     for (int i = 0; i < size; i++) {
         mass[i] = rand() % 99;
     }
@@ -21,16 +22,17 @@ int main()
     cin >> Del_Num;
 
     for (int i = 0; i < size; i++) {
-           if (mass[i] == Del_Num) {
+        if (mass[i] == Del_Num) {
             Col++;
         }
-           if (mass[i] % 2 == 0 && mass[i] != Del_Num) {
+        if (mass[i] % 2 == 0 && mass[i] != Del_Num) {
             Even_Num++;
         }
     }
-    int* New_Mass = new int[size - Col + Even_Num];
+    int New_Size = size - Col + Even_Num;
+    int* New_Mass = new int[New_Size];
     int j = 0;
-       for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         if (mass[i] != Del_Num) {
             if (mass[i] % 2 == 0) {
                 New_Mass[j++] = 1;
@@ -39,10 +41,9 @@ int main()
         }
     }
     delete[]mass;
-    mass = New_Mass; 
-    for (int i = 0; i < size - Col + Even_Num; i++) {
+    mass = New_Mass;
+    for (int i = 0; i < New_Size; i++) {
         cout << mass[i] << " ";
     }
-    delete[] mass;
     cout << endl;
 }
