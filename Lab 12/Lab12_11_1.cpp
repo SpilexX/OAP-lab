415270dd50802881c4143f957ac0fd5bbc098122
Timur <spilex.dev@gmail.com>
2020-12-15 04:49:40 +0300

    Lab12_11

---
+++ Lab 12/Lab12_11_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    int i, n;
+    cout << "Введите n = ";
+    cin >> n;
+    bool* A = new bool[n+1]; 
+    //по-умолчанию все элементы такого массива равны true (205)
+        
+    for (i = 0; i < n; i++)
+    {
+        int k;
+        cout << "Введите натуральное число: ";
+        cin >> k;
+        *(A + k) = 1;
+    }
+
+    for (i = 1; i <= n; i++)
+    {
+        if (*(A + i) != 1)
+            break;
+    }
+    cout << "Отсутствующий элемент: " << i;
+}
---
+++ Lab 12/Lab12_11_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    const int size = 20;
+    int z = 50,
+        Az = 0,
+        Bz = 0;
+    int A[size], B[size];
+    cout << "z = " << z << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        *(A + i) = rand() % 100; //заполняем рандомом
+        if (*(A + i) < z) //считаем значения меньше z
+            Az++;
+       *(B + i) = rand() % 100;
+        if (*(B + i) < z)
+            Bz++;
+    }
+
+    int *C1, *C2;
+    if (Az >= Bz)
+    {
+        C1 = B;
+        C2 = A;
+        cout << "Az >= Bz";
+    }
+    else
+    {
+        C1 = A;
+        C2 = B;
+        cout << "Az < Bz";
+    }
+
+    cout << endl << "Array #1: ";
+    for (int i = 0; i < size; i++)
+        cout << *(C1 + i) << ' ';
+
+    cout << endl << "Array #2: ";
+    for (int i = 0; i < size; i++)
+        cout << *(C2 + i) << ' ';
+}
